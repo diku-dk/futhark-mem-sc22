59583bd760098ee50531c0497ce505c680c7283a
Troels Henriksen <athas@sigkill.dk>
2022-04-08 11:40:16 +0200

    Try this kind of shell.nix.

---
+++ shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/2e8743b8e53638d8af54c74c023e0bb317557afb.tar.gz") {} }:
+
+# Use host C compiler to avoid breaking OpenCL.
+pkgs.stdenvNoCC.mkDerivation {
+  name = "futhark-mem-sc22";
+  nativeBuildInputs = [
+     pkgs.python3
+  ];
+}
