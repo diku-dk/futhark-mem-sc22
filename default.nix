127b4493a506e641a8297d051485c412054674ba
Philip Munksgaard <philip@munksgaard.me>
2022-04-09 00:01:46 +0200

    Set default tag to "cuda"

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let
                   cp futhark-nightly/bin/futhark $out
                 '';
   });
-  nixFromDockerHub = pkgs.dockerTools.pullImage {
+  cuda = pkgs.dockerTools.pullImage {
     imageName = "nvidia/cuda";
     imageDigest = "sha256:833fe2344bbe88181e79758fbcc744721f79ff2f6f7669a92020200bbd7445f2";
     sha256 = "0mv3nhfjjv49h9yd30fn3x14087dwpdc4n4aci29ak8difqf41jf";
@@ -19,8 +19,8 @@ let
 in
 pkgs.dockerTools.buildLayeredImage {
   name = "futhark-mem-sc22";
-  tag = "latest";
-  fromImage = nixFromDockerHub;
+  tag = "cuda";
+  fromImage = cuda;
 
   contents = [futhark
               benchmarks
