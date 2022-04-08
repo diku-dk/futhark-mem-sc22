0d35c80d7bf3e4b162efd4f04e3deab34b439db3
Philip Munksgaard <philip@munksgaard.me>
2022-04-08 09:22:29 +0200

    Use asci tables instead

--- Makefile
+++ Makefile
@@ -1,35 +1,35 @@
-outputs/tables.pdf: outputs/tables.tex outputs/table1.tex outputs/table2.tex outputs/table3.tex outputs/table4.tex outputs/table5.tex outputs/table6.tex
-	cd outputs && pdflatex $@
+.PHONY: all table1 table2 table3 table4 table5 table6
+all: table1 table2 table3 table4 table5 table6
 
-outputs/table1.tex:
+table1:
 	make -C benchmarks/nw/futhark all
 	make -C benchmarks/nw/reference results.json
-	tools/result-table.py benchmarks/nw > $@
+	tools/result-table.py benchmarks/nw
 
-outputs/table2.tex:
+table2:
 	make -C benchmarks/lud/futhark all
 	make -C benchmarks/lud/reference results.json
-	tools/result-table.py benchmarks/lud > $@
+	tools/result-table.py benchmarks/lud
 
-outputs/table3.tex:
+table3:
 	make -C benchmarks/hotspot/futhark all
 	make -C benchmarks/hotspot/reference results.json
-	tools/result-table.py benchmarks/hotspot > $@
+	tools/result-table.py benchmarks/hotspot
 
-outputs/table4.tex:
+table4:
 	make -C benchmarks/lbm/futhark all
 	make -C benchmarks/lbm/reference results.json
-	tools/result-table.py benchmarks/lbm > $@
+	tools/result-table.py benchmarks/lbm
 
-outputs/table5.tex:
+table5:
 	make -C benchmarks/OptionPricing/futhark all
 	make -C benchmarks/OptionPricing/reference results.json
-	tools/result-table.py benchmarks/OptionPricing > $@
+	tools/result-table.py benchmarks/OptionPricing
 
-outputs/table6.tex:
+table6:
 	make -C benchmarks/LocVolCalib/futhark all
 	make -C benchmarks/LocVolCalib/reference results.json
-	tools/result-table.py benchmarks/LocVolCalib > $@
+	tools/result-table.py benchmarks/LocVolCalib
 
 
 .PHONY: clean
--- tools/result-table.py
+++ tools/result-table.py
@@ -15,7 +15,7 @@ def canonical_name(name):
     try:
         return str(max(list(map(int, re.findall(r'\d+', name)))))
     except:
-        return list(filter(lambda x: x, name.split("/")))[-1]
+        return list((list(filter(lambda x: x, name.split("/")))[-1]).split("."))[0]
 
 if __name__ == '__main__':
     _, bench = sys.argv
@@ -38,39 +38,21 @@ if __name__ == '__main__':
 
     name = list(filter(lambda x: x, bench.split("/")))[-1]
 
-    print("""
-\\begin{{table}}[!t]
-  \\renewcommand{{\\arraystretch}}{{1.3}}
-  \\caption{{{name} Performance ({num} runs)}}
-  \\label{{tab:{name}-performance-32}}
-  \\centering
-  \\begin{{tabular}}{{c||c||c||c||c||c}}
-    \\hline
-    \\bfseries Dataset & \\bfseries Reference & \\bfseries \\thead{{Unopt. \\\\ Futhark}} & \\bfseries \\thead{{Opt. \\\\ Futhark}} & \\bfseries \\thead{{Opt. \\\\ Impact}} & \\bfseries \\thead{{Mem. \\\\ Red. }} \\\\
-    \\hline\\hline
-""".format(name = name,
-           num = len(list(plain_json.values())[0]["runtimes"])))
+    print("\n{name} Performace ({num} runs)\n"
+          .format(name = name,
+                  num = len(list(plain_json.values())[0]["runtimes"])))
+    print(" Dataset  | Reference | Unopt. Futhark | Opt. Futhark | Opt. Impact")
+    print("----------+-----------+----------------+--------------+------------")
 
     for dataset, results in plain_json.items():
         pretty_name = canonical_name(dataset)
 
         ref = np.mean(reference[dataset]["runtimes"])
-        print('    %% {name} & {reference_us} & {plain_us} & {optimized_us} & {impact}\\\\\n'
-              '    {name} & {reference:d}ms & {plain_speedup:.2f}x & {optimized_speedup:.2f}x & {impact:.2f}x & {mem:.0f}\\% \\\\'
+        print(' {name:<8} | {reference:>7d}ms | {plain_speedup:>13.2f}x | {optimized_speedup:>11.2f}x | {impact:>10.2f}x'
               .format(name = pretty_name,
                       reference = int(round(ref / 1000)),
-                      reference_us = ref,
-                      plain = int(round(np.mean(results["runtimes"]) / 1000)),
-                      plain_us = np.mean(results["runtimes"]),
                       plain_speedup = ref / np.mean(results["runtimes"]),
-                      optimized = int(round(np.mean(optimized_json[dataset]["runtimes"]) / 1000)),
-                      optimized_us = np.mean(optimized_json[dataset]["runtimes"]),
                       optimized_speedup = ref / np.mean(optimized_json[dataset]["runtimes"]),
-                      impact = np.mean(results["runtimes"]) / np.mean(optimized_json[dataset]["runtimes"]),
-                      mem = (1 - optimized_json[dataset]["bytes"] / results["bytes"]) * 100))
+                      impact = np.mean(results["runtimes"]) / np.mean(optimized_json[dataset]["runtimes"])))
 
-    print("""
-    \\hline
-  \\end{tabular}
-\\end{table}
-""")
+    print("")
