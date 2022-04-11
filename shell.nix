afb86a4e51ac3444f1e575591e4ae8f2459fd26e
Philip Munksgaard <philip@munksgaard.me>
2022-04-11 12:07:58 +0200

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,18 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/2e8743b8e53638d8af54c74c023e0bb317557afb.tar.gz") {} }:
 
+let python-packages = python-packages: with python-packages; [
+    numpy
+    ];
+in
 # Use host C compiler to avoid breaking OpenCL.
 pkgs.stdenvNoCC.mkDerivation {
   name = "futhark-mem-sc22";
   nativeBuildInputs = [
      pkgs.python2
-     pkgs.python3
+     (pkgs.python39.withPackages python-packages)
      pkgs.moreutils
      pkgs.jq
+     pkgs.gnumake
+     pkgs.bash
   ];
 }
